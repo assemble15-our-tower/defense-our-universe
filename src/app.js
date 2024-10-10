277d02ead996e70c44c85e5ff84a7481489d9817
Chong-Chongs <jojia1211@gmail.com>
2024-10-10 12:31:57 +0900

    app.js 포트 방식 변경
    app.js 포트 .env로 빼고 불러오는 방식으로 변경

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 const server = createServer(app);
-const PORT = 9999;
+const PORT = process.env.DATABASE_PORT;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended : false }));
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+  res.send('Hello world!');
+});
 
 server.listen(PORT, async () => {
-    console.log(`Server is running on port ${PORT}`)
-    // 서버가 시동될때 CDN을 읽어야 하니까 여기쯤에 파일 리딩 구문이 들어가야 한다
-try {
+  console.log(`Server is running on port ${PORT}`);
+  // 서버가 시동될때 CDN을 읽어야 하니까 여기쯤에 파일 리딩 구문이 들어가야 한다
+  try {
     const assets = await loadGameAssets();
     console.log(assets);
     console.log('Assets loaded successfully');
-} catch (e) {
-    console.error('Failed to load game assets: ', e)
-}
+  } catch (e) {
+    console.error('Failed to load game assets: ', e);
+  }
 });
