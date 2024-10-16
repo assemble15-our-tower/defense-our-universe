17a62db3ae90aca6f3f9694cb2a46c7b267f7a3b
liha-engineer <liha.engineer@gmail.com>
2024-10-16 17:05:34 +0900

    refactor: 17번 몬스터 처치 핸들러 이름 수정

--- src/handlers/handlerMapping.js
+++ src/handlers/handlerMapping.js
@@ -1,13 +1,13 @@
 import { gameEnd, gameStart } from './game.handler.js';
 import { moveStageHandler } from './stage.handler.js';
 import { bindTower } from './tower.handler.js';
-import { handleEliminateMonster } from './monster.handler.js'
+import { eliminateMonster } from './monster.handler.js'
 
 const handlerMappings = {
   2: gameStart,
   3: gameEnd,
   11: moveStageHandler,
-  17: handleEliminateMonster,
+  17: eliminateMonster,
   99: bindTower
 
 };
